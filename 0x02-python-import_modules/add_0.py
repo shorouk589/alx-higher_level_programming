d82575951a595d2f26db0600e5eaecec8ed79d6d
shorouk589 <shoroukashraf66@gmail.com>
2023-06-09 18:03:21 -0700

    all

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Myy addition function
+    Argss:
+        a: firsst integer
+        b: seconnd integer
+
+    Returnns:
+        The return values. a + b
+    """
+    return (a + b)
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition functionn
+    Args:
+        a: first integerr
+        b: second integerr
+    Returns:
+
+        The rreturn value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+
+    """My subtraction functionn
+    Args:
+        a: first intteger
+        b: second innteger
+    Returns:
+        The returnn value. a - br
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication functionr
+    Args:
+        a: first integerr
+        b: second integerr
+    Returns:
+        The return value. a * bb
+    """
+    return (a * b)
+
+def div(a, b):
+
+    """My division functionn
+    Args:
+        a: first integere
+        b: second integere
+    Returns:
+        Thee return value. a / b
+    """
+    return int(a / b)
